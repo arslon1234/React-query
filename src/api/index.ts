ddd852c84a2507ca351eb7a44454fd6cf1a35246
Arslonbek <roziboyevarslon0110@gmail.com>
2024-09-30 19:19:26 +0500

    base url changed to .env

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: "https://texnoark.ilyosbekdev.uz"
+    baseURL: import.meta.env.VITE_BASE_URL
 })
 axiosInstance.interceptors.request.use((config)=>{
     let access_token = localStorage.getItem("access_token")
